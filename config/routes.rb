ee9928a6c0c3f6231c63a059fb16c90b3517a38d
EC2 Default User <ec2-user@ip-172-31-27-127.ec2.internal>
2021-11-11 13:46:59 +0000

    [Finish] 2-9

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   get 'todolists/:id' => 'todolists#show', as: 'todolist'
   get 'todolists/:id/edit' => 'todolists#edit', as: 'edit_todolist'
   patch 'todolists/:id' => 'todolists#update', as: 'update_todolist'
+  delete 'todolists/:id' => 'todolists#destroy', as: 'destroy_todolist'
 #GETは、aタグで画面遷移する場合に使う。
 #POSTは、formタグ等でデータ送信する場合に使う。
 
